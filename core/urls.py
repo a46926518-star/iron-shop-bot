8817552e1589393b24955fa530d836e22d7d8ea5
a46926518-star <a46926518@gmail.com>
2026-05-13 17:42:46 +0500

    Fix core\urls and all

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from api import views as api_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 
