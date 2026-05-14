51946419d83df70d63798bbaab19fa293cb9cbc4
a46926518-star <a46926518@gmail.com>
2026-05-14 14:41:41 +0500

    Home va Kanban yo'llari to'g'rilandi

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from api import views as api_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='registration/home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 
